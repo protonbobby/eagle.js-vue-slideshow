aa6d96ef43ae0607e16f3bde81a51773fa465ef1
bobby <bobby@buildinglink.com>
2021-03-22 08:03:49 -0400

    deployment script added

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   lintOnSave: 'error',
   // runtimeCompiler: process.env.NODE_ENV !== 'production'
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/eaglejs-vue-slideshow/'
+  : '/'
 }
